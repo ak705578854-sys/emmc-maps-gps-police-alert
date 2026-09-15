2c8c73df74da7913bdc893760482aeb2db5965b5
ak705578854-sys <ak705578854@gmail.com>
2026-09-15 09:02:15 +0530

    Add multiple ambulance IDs to config

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const BACKEND_URL = "https://emmc-push-backend.onrender.com";
-
 export const AMBULANCE_ID = "AMB102";
+export const AMBULANCE_IDS = ["AMB102", "AMB103", "AMB104", "AMB105", "AMB106"];
 export const AUTHORIZED_POLICE_ID = "TP001";
 export const POLICE_ALERT_RADIUS_KM = 1;
