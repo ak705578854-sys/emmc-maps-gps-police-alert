09e691f964f091937af46c0107b327990db0856a
ak705578854-sys <ak705578854@gmail.com>
2026-09-19 17:19:19 +0530

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// EMMC production real-GPS configuration.
-export const BACKEND_URL = "https://emmc-push-backend.onrender.com";
-export const AMBULANCE_ID = "";
-export const AMBULANCE_IDS = [];
-export const AUTHORIZED_POLICE_ID = "TP001";
-export const POLICE_ALERT_RADIUS_KM = 1;
